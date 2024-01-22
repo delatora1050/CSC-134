9eec1181cb2b8dec0afe71b5adff4c42e3196c2a
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-01-22 16:16:27 +0000

    Cpmpleted M1T2

---
+++ M1T2_DeLaTorre.cpp
@@ -0,0 +1,28 @@
+// CSC 134 
+//M1T2
+// De La Torre, Alestacia
+// 20240122
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  cout << "Hello World!" << endl;
+  cout << "My Name is Alestacia De La Torre.";
+  cout << "Currently I'm enrolled in the Computer Programming Course." << "\n";
+  cout << "Some things to know about me is that I work at a gym";
+  cout << " and trying my best to become a Software Developer!" << endl;
+  
+  //Ask a question
+  string name;
+  cout << "What's your name?" << endl;
+  cin >> name;
+
+  cout << "Hi " << name << ", nice to meet you!"<< endl;
+
+
+  cout << endl << endl;
+  return 0;
+}
