9c771ee3f8e234b0b7c48282f658b084a5da2b28
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-01-29 16:18:40 +0000

    Clean up

---
+++ M1/M1T1_DeLaTorre.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online(oneline comment)
+/*
+multi
+line
+comment 
+*/
+/*
+CSC134
+De La Torre, Alestacia
+20240117
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    // for now we'll introduce ourselves
+    // printyour name
+    cout << "Hello , my name is Alestacia De La Torre." <<endl;
+    
+    // print yur favorite pokemon or vehicle
+    cout << "My favorite Pokémon is Gengar." << endl;
+    
+    return 0;
+}
