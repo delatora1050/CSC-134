8bc989ff7a5e89513925da033a6f0d29caf79720
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-12 04:53:23 +0000

    v2

--- M2/M2HW1_#1_DeLaTorre.cpp
+++ M2/M2HW1_#1_DeLaTorre.cpp
@@ -7,13 +7,27 @@ int main() {
     string name;
     double num_dep;
     double num_with;
+    int acct_num = 158763;
+    double acct_bal= 0;
+    double total;
 
-    cout << "Hello! What is the name on the account?" << endl;
+    cout << "Hello! What is the name on the account? ";
     cin >> name;
 
-    cout << "How much would you like to deposit?" <<endl;
+    cout << "How much would you like to deposit? ";
     cin >> num_dep;
 
+    cout << " How much would you like to widthdraw? ";
+    cin >> num_with;
 
+    acct_bal = (num_dep + acct_bal);
+    acct_bal = (acct_bal - num_with);
+    total = acct_bal;
+
+
+    cout << "Printed is " << name << " account summary" << endl;
+    cout << "Your Account number is " << acct_num << endl;
+    cout << "Your current balance is " << total << endl;
+    
     return 0;
 }
