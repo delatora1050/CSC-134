68a03e063da618f9032b9aba1e512b74b22f1085
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-12 04:17:21 +0000

    v1

---
+++ M2/M2HW1_#1_DeLaTorre.cpp
@@ -0,0 +1,19 @@
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    double num_dep;
+    double num_with;
+
+    cout << "Hello! What is the name on the account?" << endl;
+    cin >> name;
+
+    cout << "How much would you like to deposit?" <<endl;
+    cin >> num_dep;
+
+
+    return 0;
+}
