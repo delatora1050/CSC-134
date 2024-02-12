af753c89372ae515988138171da8706c08cd8529
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-12 17:33:46 +0000

    Final-ish

--- M2/M2HW1_#3_DeLaTorre.cpp
+++ M2/M2HW1_#3_DeLaTorre.cpp
@@ -13,6 +13,7 @@ int main() {
     int slice_per_pizza;
     int left_over_pizza;
     int slice_per_person = 3;
+    int temp;
 
     cout << "Hello and welcome! How many guest will you be having? " << endl;
     cin >> amt_people;
@@ -20,11 +21,15 @@ int main() {
     cout << "Awesome! How many pizza's will you like to order? " << endl;
     cin >> amt_pizza;
 
-    count << "And how many slices per pizza would you like? " << endl;
-    cin >> slices_per_pizza;
+    cout << "And how many slices per pizza would you like? " << endl;
+    cin >> slice_per_pizza;
 
-    left_over_pizza = (slice_per_person * amt_people) - slice_per_pizza;
+    temp = slice_per_pizza * amt_pizza;
+    left_over_pizza = (slice_per_person * amt_people) - temp;
 
     cout << "It looks like the amount of pizza left over is " << left_over_pizza << "." << endl;
-    
+    cout << endl;
+
+
+    return 0;
 }
