56a6c199b96eee5b80d438e9bbb89b7c94ff5f61
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-14 15:23:40 +0000

    starters

---
+++ M2/M2HW1_#4_DeLaTorre.cpp
@@ -0,0 +1,19 @@
+/*
+CSC - 134
+M2HW1 #2
+De La Torre,Alestacia
+20240211
+*/
+
+#include <iostream>
+#include <strings>
+using namespace std;
+int main() {
+
+    string letsGo, school, team, cheerOne, cheerTwo;
+    
+
+
+
+    return 0;
+}
