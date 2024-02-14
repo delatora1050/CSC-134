870d7c50e1f7edf280c9e0739d96f9f91bfd8554
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-14 15:30:14 +0000

    M2T1 start and template design

---
+++ M3/M3T1_DeLaTorre.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M2T1 - 
+De La Torre, Alestacia
+20240214
+*/
+
+
+// Ask for width, length of first triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+
+}
---
+++ starter.cpp
@@ -0,0 +1,17 @@
+/*
+CSC 134
+M2T1 - 
+De La Torre, Alestacia
+20240214
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+
+}
