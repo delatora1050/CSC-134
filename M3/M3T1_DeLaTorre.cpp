8363301b3dc65b9ae23ade95ab8d3b432281ab94
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-14 15:57:40 +0000

    V3

--- M3/M3T1_DeLaTorre.cpp
+++ M3/M3T1_DeLaTorre.cpp
@@ -16,24 +16,59 @@ int main()
     // Declare var 
     double base, height;
     double base_2, height_2;
-    double area, area_2;
+    double area_1, area_2;
 
     // Ask for width, length of first rectangle
     cout << "Hello and welcome!" << endl;
     cout << "What is the width of the first rectangle? ";
     cin >> base;
 
-    cout << "What is is the length of the first rectangle? ";
+    cout << "What is is the height of the first rectangle? ";
     cin >> height;
 
     // calculate area of first rectangle
-    area = (base * height);
+    area_1 = (base * height);
 
     // print the area
-    cout << "The area of the first rectangle is " << area << endl;
+    cout << "The area of the first rectangle is " << area_1 << endl;
     cout << endl;
 
-    
+    // 2
+    cout << "Please enter the base and height of second rectangle below." << endl;
+
+    // input for 2nd rectangle
+    cout << "What is the base of the second rectangle? ";
+    cin >> base_2;
+
+    cout << "What is the height of the second rectangle? ";
+    cin >> height_2;
+
+    // Cal 2nd rectangle area
+    area_2 = (base_2 * height_2);
+
+    cout << "The area of the second rectangle is " << area_2 << endl;
+    cout << endl;
+
+    if (area_1 > area_2)
+    {
+        cout << "The area of the first rectangle is greater than the second rectangle. " << endl;
+        cout << endl;
+    }
+    else if (area_1 < area_2)
+    {
+        cout << "The area of the first rectangle is less than the second rectangle. " << endl;
+        cout << endl;
+    }
+    else if (area_1 == area_2)
+    {
+        cout << "The area of the first rectangle is equal to the second rectangle. " << endl;
+        cout << endl;
+    }
+    else 
+    {
+        cout << ("Something went wrong!") << endl;
+        cout << endl;
+    }
 
 
 
