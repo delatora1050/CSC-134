e14fc12e3ee7cc37c9b21e29ebf87d34450d6b0c
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-14 15:44:39 +0000

    v2

--- M3/M3T1_DeLaTorre.cpp
+++ M3/M3T1_DeLaTorre.cpp
@@ -13,6 +13,31 @@ using namespace std;
 
 int main()
 {
+    // Declare var 
+    double base, height;
+    double base_2, height_2;
+    double area, area_2;
+
+    // Ask for width, length of first rectangle
+    cout << "Hello and welcome!" << endl;
+    cout << "What is the width of the first rectangle? ";
+    cin >> base;
+
+    cout << "What is is the length of the first rectangle? ";
+    cin >> height;
+
+    // calculate area of first rectangle
+    area = (base * height);
+
+    // print the area
+    cout << "The area of the first rectangle is " << area << endl;
+    cout << endl;
+
+    
+
+
+
+
     return 0;
 
 }
