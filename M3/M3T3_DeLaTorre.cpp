7c5787d65bf5b593ed6c9cd5f428ff166504cbb3
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-21 16:30:23 +0000

    V3

--- M3/M3T3_DeLaTorre.cpp
+++ M3/M3T3_DeLaTorre.cpp
@@ -9,6 +9,7 @@ De La Torre, Alestacia
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
@@ -17,30 +18,37 @@ int main()
     // Set up Var
     int roll;
     int die_1, die_2;
+    int seed;
+
+
+    // Set random seed
+    //cout << "Enter you 🍀 lucky number: ";
+    //cin >> seed;
+    //srand(seed);
     srand(time(0));
 
 
     // ROLL Dice
     //cout << "What did you roll? ";
-   //cin >> roll;
-   die_1 = rand() % 6 + 1;
-   die_2 = rand() % 6 + 1;
+    //cin >> roll;
+    die_1 = rand() % 6 + 1;
+    die_2 = rand() % 6 + 1;
     roll = die_1 + die_2;
 
     // Results
-    cout << "Your roll was: ( " << die_1 << " + " << die_2 << " ) -> " << roll << endl;
+    cout << "Your roll was: ( 🎲 " << die_1 << " + 🎲 " << die_2 << " ) -> " << roll << endl;
     if (roll == 7 || roll == 11){
         cout << "You Win!!" << endl;
     }
     else if ( roll == 2 || roll == 3 || roll == 12){
-        cout << "You lose..." << endl;
+        cout << "You lose \342\230\272 ..." << endl;
     }
     else {
         cout << "Your point is: " << roll << endl;
         cout << "TODO: roll the point" << endl;
     }
 
-
+    cout << endl;
 
 
 
