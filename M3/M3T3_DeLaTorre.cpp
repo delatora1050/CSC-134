0ca008b408d5b258df7ea307efa1053106148332
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-21 16:06:29 +0000

    V2

--- M3/M3T3_DeLaTorre.cpp
+++ M3/M3T3_DeLaTorre.cpp
@@ -8,19 +8,42 @@ De La Torre, Alestacia
 
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
     cout << "Welcome to the Craps Table" << endl;
-    cout << "What did you roll? ";
-    // cin >> roll;
+    // Set up Var
+    int roll;
+    int die_1, die_2;
+    srand(time(0));
+
+
+    // ROLL Dice
+    //cout << "What did you roll? ";
+   //cin >> roll;
+   die_1 = rand() % 6 + 1;
+   die_2 = rand() % 6 + 1;
+    roll = die_1 + die_2;
+
+    // Results
+    cout << "Your roll was: ( " << die_1 << " + " << die_2 << " ) -> " << roll << endl;
+    if (roll == 7 || roll == 11){
+        cout << "You Win!!" << endl;
+    }
+    else if ( roll == 2 || roll == 3 || roll == 12){
+        cout << "You lose..." << endl;
+    }
+    else {
+        cout << "Your point is: " << roll << endl;
+        cout << "TODO: roll the point" << endl;
+    }
 
 
 
 
 
-    
     return 0;
 
 }
