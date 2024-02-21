c4d2c752190664654b4b202b0cae8e529e694200
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-02-21 15:40:59 +0000

    M3T3 - Craps Tables

---
+++ M3/M3T3_DeLaTorre.cpp
@@ -0,0 +1,26 @@
+/*
+CSC 134
+M3T3 - Craps 
+De La Torre, Alestacia
+20240221
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to the Craps Table" << endl;
+    cout << "What did you roll? ";
+    // cin >> roll;
+
+
+
+
+
+    
+    return 0;
+
+}
